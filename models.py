db0533347f671c901f1cac0b9eefed05e6fef42c
envi <alexgolubev1404@gmail.com>
2024-12-28 16:59:58 +0700

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,3 @@ from flask_login import UserMixin
 
 db = SQLAlchemy()
 
-class Users(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=T
