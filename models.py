4910fd9f7c1711a5cd37791b336b94a39270ccd8
envi <alexgolubev1404@gmail.com>
2024-12-28 16:55:50 +0700

    Откат до нуля

--- models.py
+++ models.py
@@ -6,19 +6,4 @@ db = SQLAlchemy()
 class Users(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    is_active = db.Column(db.Boolean, default=True)
-    phone = db.Column(db.String(10), nullable=True)
-    telegram_id = db.Column(db.String(256), nullable=True, unique=True)
-    telegram_notifications = db.Column(db.Boolean(), default=True)
-
-class Admins(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    is_active = db.Column(db.Boolean, default=True)
-    phone = db.Column(db.String(10), nullable=True)
-    telegram_id = db.Column(db.String(256), nullable=True, unique=True)
-    telegram_notifications = db.Column(db.Boolean(), default=True)
+    email = db.Column(db.String(120), unique=T
