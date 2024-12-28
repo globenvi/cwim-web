0c4528b44c89ad5ad8111033147adfa5b386d66e
envi <alexgolubev1404@gmail.com>
2024-12-28 07:06:56 -0800

    test timeweb

--- req_gen.py
+++ req_gen.py
@@ -1,15 +1,10 @@
 import subprocess
-import platform
-import sys
 
 def create_requirements_file(output_file="requirements.txt"):
     try:
-        # Определяем команду для pip в зависимости от ОС
-        pip_command = "pip" if platform.system() != "Windows" else "pip3"
-
         # Получаем список установленных пакетов
         result = subprocess.run(
-            [pip_command, "freeze"],
+            ["pip", "freeze"],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
@@ -17,7 +12,7 @@ def create_requirements_file(output_file="requirements.txt"):
         
         # Проверяем на наличие ошибок
         if result.returncode != 0:
-            print(f"Ошибка при выполнении {pip_command} freeze: {result.stderr}")
+            print(f"Ошибка при выполнении pip freeze: {result.stderr}")
             return
         
         # Записываем результат в файл
