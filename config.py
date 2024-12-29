5c042c83eb581c502241db49e587a71f2ee29d7a
envi <alexgolubev1404@gmail.com>
2024-12-29 09:33:33 -0800

    modified:   config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ import os
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'jklhdaisuhiuqh9138h98dhsa898dh1hukdashjk')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
