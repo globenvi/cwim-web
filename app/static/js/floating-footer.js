988a263f1bb8915d619adee7cedf94efe2993b1a
envi <alexgolubev1404@gmail.com>
2024-12-29 02:45:30 -0800

    update design

---
+++ app/static/js/floating-footer.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", () => {
+    const footer = document.querySelector(".floating-footer");
+
+    // Добавляем класс "show" для отображения футера
+    setTimeout(() => {
+        footer.classList.add("show");
+    }, 300); // 1000 миллисекунд (1 секунда) задержки
+
+    // Закрытие футера
+    const closeButton = document.querySelector(".close-footer");
+    closeButton.addEventListener("click", () => {
+        footer.classList.remove("show");
+    });
+});
