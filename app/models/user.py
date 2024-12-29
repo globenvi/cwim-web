02647f6816e22bdf7b1341080b4a0192080abbe6
envi <alexgolubev1404@gmail.com>
2024-12-29 11:29:09 -0800

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -4,8 +4,8 @@ from app.extensions import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     telegram_id = db.Column(db.Integer, unique=True)
-    first_name = db.Column(db.String(50), nullable=False)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    first_name = db.Column(db.String(50), nullable=True)
+    username = db.Column(db.String(50), unique=True, nullable=True)
     language_code = db.Column(db.String(10), nullable=False)
     is_premium_tg = db.Column(db.Boolean, default=False)
-    photo_url = db.Column(db.String(255))
+    photo_url = db.Column(db.String(255), nullable=True)
