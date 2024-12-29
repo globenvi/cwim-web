48b8b344ff0df252b3e061d7a04bbcb03b854768
envi <alexgolubev1404@gmail.com>
2024-12-29 09:18:25 -0800

    Test auth

--- app/blueprints/index.py
+++ app/blueprints/index.py
@@ -1,8 +1,9 @@
 # Маршрут тестового Blueprint
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 index = Blueprint("index", __name__)
 
 @index.route("/")
 def index_page():
-    return render_template('index.html')
+    return render_template('index.html', user=current_user)
