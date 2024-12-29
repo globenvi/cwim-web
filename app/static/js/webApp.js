0d066348ef97ed58b5a477055a07ef3f39951283
envi <alexgolubev1404@gmail.com>
2024-12-28 23:50:39 -0800

    Update webApp.js

--- app/static/js/webApp.js
+++ app/static/js/webApp.js
@@ -67,15 +67,8 @@ function manageBackButton() {
         localStorage.setItem('previousUrl', currentPath); // Сохраняем текущий путь
         Telegram.WebApp.BackButton.show();
         Telegram.WebApp.BackButton.onClick(function () {
-            const previousUrl = localStorage.getItem('previousUrl'); // Извлекаем предыдущий URL
-            if (previousUrl) {
-                // Используем pushState для корректного изменения URL
-                history.pushState({}, '', previousUrl); // Обновляем URL, но без перезагрузки страницы
-                // Дополнительно можно использовать history.replaceState(), если нужно не добавлять запись в историю
-            } else {
-                // Если не найден URL, просто возвращаемся в историю
-                window.history.back();
-            }
+            // Загружаем страницу заново
+            window.location.reload(); // Перезагружаем страницу для возврата на предыдущий URL
         });
     } else {
         // На главной странице кнопка "Назад" не отображается
