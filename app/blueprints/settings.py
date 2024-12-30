30d8cd71ab7642c655f144691cb54023003fa419
envi <alexgolubev1404@gmail.com>
2024-12-29 20:49:58 -0800

    modified:   app/blueprints/__pycache__/settings.cpython-313.pyc 	modified:   app/blueprints/settings.py 	modified:   app/templates/settings.html

--- app/blueprints/settings.py
+++ app/blueprints/settings.py
@@ -1,9 +1,10 @@
 # Маршрут тестового Blueprint
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, redirect, request, flash
+from flask_login import login_required, current_user
 
 settings = Blueprint("settings", __name__)
 
 @settings.route("/")
 def settings_page():
     # Тестовый маршрут возвращает строку "Hello, Flask!"
-    return render_template('settings.html')
+    return render_template('settings.html', user=current_user)
