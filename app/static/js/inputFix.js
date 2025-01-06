30d8a11b4c0e12b9c6a3b813af6473ab088e8b24
envi <alexgolubev1404@gmail.com>
2025-01-06 18:20:35 +0700

    update

--- app/static/js/inputFix.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('click', function(event) {
-    const inputs = document.querySelectorAll('input, textarea');
-    inputs.forEach(input => {
-        if (!input.contains(event.target) && input === document.activeElement) {
-            input.blur();
-        }
-    });
-});
